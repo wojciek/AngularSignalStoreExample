55c90c96ba87ab5b11a6cb18e46ab3f5738a9b25
wojciek <wojtekgluch@gmail.com>
2024-07-19 10:26:44 +0200

    Create store.store.ts
    store added

---
+++ store.store.ts
@@ -0,0 +1,36 @@
+export type DataModelState = {
+  id: number | undefined;
+  data: DataModel;
+  isLoading: boolean;
+  errorMessage: string;
+};
+
+export const DataModelStore = signalStore(
+  withState<DataModelState>({ id: undefined, returnForm: new DataModel(), isLoading: false, errorMessage: '' }),
+  withDataModelSelectors(),
+  withDataModelMethods(),
+  dataModelWithAdditionalData(),
+  withCustomLogger('AdditionalInformationAddedToLoggerAsIdOrSth'),
+  withHooks({
+    onInit(store) {
+      const dataModelIdToLoad = store.id();
+      if (dataModelIdToLoad) {
+        store.loadDataModel(dataModelIdToLoad);
+      } else {
+        store.createNewDataModel(null);
+        //store.loadReturnForm(draftRmaRequestToLoad);
+      }
+      // Load installers only if needed
+      store.data.additionalDataRequired ?? store.loadAdditionalData(null);
+      console.log('Store initialized');
+      // Use it for logging changes or just use withHubLogger()
+      // effect(() => {
+      //   //const loadingService = inject(LoadingService);
+      //   const state = getState(store);
+      //   console.log(`${name} state changed`, state);
+      //   // There we can put toast service or loding service.
+      // });
+    },
+    onDestroy: store => console.log('Store destroyed', store),
+  })
+);
