e40250f11a59baf3c61b60ce8663756b261d20f6
wojciek <wojtekgluch@gmail.com>
2024-07-19 10:11:24 +0200

    Create store.logger.ts
    Custom logger added

---
+++ store.logger.ts
@@ -0,0 +1,11 @@
+export function withCustomLogger(name: string) {
+  return signalStoreFeature(
+    withHooks({
+      onInit(store) {
+        effect(() => {
+          const state = getState(store);
+          console.log(`${name} state changed: `, state);
+        });
+      },
+    })
+  );
